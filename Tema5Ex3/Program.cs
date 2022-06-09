214be060923b397f182813958409c5a1e73494aa
Gergoa123 <gergo3124@gmail.com>
2022-06-09 16:44:57 +0300

    Add project files.

---
+++ Tema5Ex3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tema5Ex3
+{
+    //Ex 3
+    //Scrieti un program care va numara toate aparitiile unui caracter intr-un sir
+    //de caractere.Atat caracterul cautat cat si sirul de caracter vor fi citite de la
+    //tastatura
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int aux = 0;
+
+            Console.WriteLine("Introduceti string-ul!");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Introduceti caracterul cautat");
+            char caracter = Console.ReadLine()[0];
+
+            for(int i = 0; i < string1.Length; i++)
+            {
+                if (string1[i] == caracter )
+                {
+                    aux++;
+
+                }
+
+            }
+            
+
+            Console.WriteLine(" Caracterul apare in string de " + aux + " ori.");
+
+        }
+    }
+}
